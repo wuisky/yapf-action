9f823526482d65e4c05a5cf331a699eaca569e39
MENGHUNG WU <m.wu@gitai.tech>
2023-03-25 19:56:32 +0900

    add reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -e
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
 echo "Checking formatting for $GITHUB_REPOSITORY"
-sh -c "yapf --diff --recursive $GITHUB_WORKSPACE $*"
+sh -c "yapf --diff --recursive $GITHUB_WORKSPACE $*" | \
+    reviewdog -name=yapf-action -f=diff -tee \
+              -reporter="${INPUT_REPORTER}" \
+              -filter-mode="${INPUT_FILTER_MODE}" \
+              -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+              -level="${INPUT_LEVEL}" \
+              ${INPUT_REVIEWDOG_FLAGS}

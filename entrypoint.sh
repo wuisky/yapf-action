94d4d1d4ea405fa3d3699f51d05fa7fad1b83fca
WU MENGHUNG <wu.menghung@jp.panasonic.com>
2023-04-07 10:48:43 +0900

    add target_path input

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo "Checking formatting for $GITHUB_REPOSITORY"
-sh -c "yapf --diff --recursive $GITHUB_WORKSPACE $*" | \
+sh -c "yapf --diff --recursive ${INPUT_TARGET_PATH} $*" | \
     reviewdog -name=yapf-action -f=diff -tee \
               -reporter="${INPUT_REPORTER}" \
               -filter-mode="${INPUT_FILTER_MODE}" \
